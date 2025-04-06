9122ef0a83dfa2eebb778f92bff56933bd78c44e
Paola Marcano <marcanosalaspv@uvm.edu.ve>
2025-04-06 00:21:01 -0400

    ajustar puertos de ejecución

--- backend/app.js
+++ backend/app.js
@@ -38,8 +38,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(3000, () => {
-  console.log('Servidor escuchando en el puerto 3000');
+app.listen(4000, () => {
+  console.log('Servidor escuchando en el puerto 4000');
 });
 
 module.exports = app;
